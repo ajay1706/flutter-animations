8a31a3b540f3f9d1653b364c7c6116b482358dcd
ajay1706 <ajay97456@gmail.com>
2019-09-19 15:23:11 +0530

    adding two animation of opacity and Size

--- flutter_animations/lib/main.dart
+++ flutter_animations/lib/main.dart
@@ -19,7 +19,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin {
   //firstly write this animation class
 //This is where animation starts(It will do all the stuff with values.)
-Animation<Color> animation; //animation object has all the state, value and the position of the animations
+Animation<double> animation; //animation object has all the state, value and the position of the animations
 // It doesn't know anything happening on screen as it is a abstract class
 //To change the color we use Color than double
 AnimationController animationController ; // Its another object which generates value & frames for the animations
@@ -34,16 +34,16 @@ AnimationController animationController ; // Its another object which generates 
 
 
              //here we are using the Tween Animation to set the begin and end duration and you can use another like Curved too.
-                 animation = ColorTween(begin: Colors.white , end: Colors.black87).animate(animationController);
+                 animation = Tween<double>(begin: 0.0 , end:1.0).animate(animationController);
    
    
    //Now we have two types of listeners for the animation.
    //ONe is .addListener() Property whuch shows the value for each frame in the animation.
-     animation.addListener((){
-       setState((){
-print(animation.value.toString());
-       });
-     });
+//      animation.addListener((){    //delete this part after creating the AnimatedLogo class
+//        setState((){
+// print(animation.value.toString());
+//        });
+//      });
 
 //Another way to add the listener to the animation is .addStatusListener() which gets the status of the animation.
  animation.addStatusListener((staus) => print(staus));
@@ -58,13 +58,35 @@ print(animation.value.toString());
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Container(
-        height: 100.0,
-        width:100.0,
-        color:animation.value,
-        child: FlutterLogo(),
-      ),
+      child:AnimatedLogo(
+        animation: animation,
+      )
       
     );
   }
+}
+//AnimatedWidget help in creating and setting states of aimation here rather than thier in the UI and logic part
+//The Purpose of using this as now we don't need the setState or addListener method
+class AnimatedLogo extends AnimatedWidget{
+final Tween<double> _sizeAnim = Tween<double>(begin:50,end: 100);
+  AnimatedLogo(
+{Key key, Animation animation}): 
+super(key:key , listenable:animation);
+
+  @override
+  Widget build(BuildContext context) {
+    final  Animation<double> animation = listenable;
+    // TODO: implement build
+    return Opacity( 
+      opacity: animation.value,
+     
+     
+      child: Container(
+        height: _sizeAnim.evaluate(animation),
+        width: 100,
+        child: FlutterLogo(),
+      ),
+    );
+  }
+
 }
